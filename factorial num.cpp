6e404cc02762832a17ef11c36d845cec94d7a6fa
Kiyog1676 <139625108+Kiyog1676@users.noreply.github.com>
2023-08-05 12:00:42 -0500

    Add files via upload

---
+++ factorial num.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int i,fact=1,n
+    printf("Enter a number to find factorial: ");
+	scanf("%d", &n);
+	
+	for (i=0;i<=n;i++);
+	
+	fact=fact*i;
+	
+	printf("factorial of %d is : %d",n,fact);
+	
+	return 0;
+	
+}
