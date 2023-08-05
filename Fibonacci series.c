1540f926c97d89e5fb3b61e480eba328fd9a0d4f
Kiyog1676 <139625108+Kiyog1676@users.noreply.github.com>
2023-08-05 12:04:50 -0500

    Add files via upload

---
+++ Fibonacci series.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+
+{
+	int n1 = 0, n2 = 1, n3, i, number;
+	
+	printf("enter the number of fibonaces:");
+	scanf("%d", &number);
+	
+	printf("\n%d%d,n1,n2");
+	
+	for(i = 2; i < number; ++i)
+	
+	return 0;
+	
+}
